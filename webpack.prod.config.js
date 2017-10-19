31497c26be8d15b997c83a379d60d609e90922c0
15271 <15271835932@163.com>
2017-10-19 21:27:00 +0800

    添加webpack打包文件

---
+++ webpack.prod.config.js
@@ -0,0 +1,82 @@
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+
+moudle.exports ={
+    entry: {
+        client: path.resolve(__dirname, 'src/index'),
+        vendor: ['babel-polyfill', 'react', 'react-dom', 'react-redux', 'react-router', 'redux', 'redux-persist', 'redux-thunk']
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            limit: 8192
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(js|jsx)$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015', 'react', 'stage-0'],
+                        plugins: ['add-module-exports','transform-object-assign',
+                            ['import', {
+                                'libraryName': 'antd',
+                                'style': 'css'
+                            }]
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
+            }
+        ]
+    },resolve: {
+        extensions: ['.js', '.json', '.jsx']
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            "process.env": {
+                NODE_ENV: JSON.stringify("production")
+            }
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: "vendor",
+            minChunks: Infinity,
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'public/index.html'
+        }),
+        new ExtractTextPlugin("styles.css"),
+        new ProgressBarPlugin()
+    ],
+    node: {
+        dgram: 'empty',
+        fs: 'empty',
+        net: 'empty',
+        tls: 'empty',
+    },
+    target: 'web'
+}
