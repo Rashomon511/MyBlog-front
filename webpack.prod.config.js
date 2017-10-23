18be93291d80eee1d8e0c9d26feb688971ed1fc8
15271 <15271835932@163.com>
2017-10-23 22:04:07 +0800

    添加webpack压缩包插件

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -61,6 +61,11 @@ module.exports = {
         extensions: ['.js', '.json', '.jsx']
     },
     plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
         new webpack.DefinePlugin({
             "process.env": {
                 NODE_ENV: JSON.stringify("production")
