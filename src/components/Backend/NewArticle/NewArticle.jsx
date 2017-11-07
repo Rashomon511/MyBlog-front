09b559566ec129b59680aeb58a545a429ad81c7a
LuoShengMen <15271835932@163.com>
2017-11-07 22:20:50 +0800

    添加编辑器

--- src/components/Backend/NewArticle/NewArticle.jsx
+++ src/components/Backend/NewArticle/NewArticle.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import PropType from 'prop-types';
-import style from './NewArticle.less'
+import style from './NewArticle.less';
+import ReactQuill from 'react-quill';
+import {modules,formats} from '../config/config'
+
 
 class NewArticle extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {editorHtml: '',}
     }
+
+    handleChange = (html) => {
+        this.setState({editorHtml: html});
+    };
+
 
     render() {
         return (
             <div>
-               newArticle
+                <ReactQuill
+                    theme="snow"  //编辑器主题
+                    value={this.state.editorHtml}
+                    onChange={this.handleChange}
+                />
             </div>
         )
     }
---
+++ src/components/Backend/config/config.js
@@ -0,0 +1,25 @@
+export const modules = {
+    toolbar: [
+        [{ 'header': '1'}, {'header': '2'}, { 'font': [] }],
+        [{size: []}],
+        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+        [{'list': 'ordered'}, {'list': 'bullet'},
+            {'indent': '-1'}, {'indent': '+1'}],
+        ['link', 'image', 'video'],
+        ['clean']
+    ],
+    clipboard: {
+        // toggle to add extra line breaks when pasting HTML:
+        matchVisual: false,
+    }
+}
+/*
+ * Quill editor formats
+ * See https://quilljs.com/docs/formats/
+ */
+export const formats = [
+    'header', 'font', 'size',
+    'bold', 'italic', 'underline', 'strike', 'blockquote',
+    'list', 'bullet', 'indent',
+    'link', 'image', 'video'
+]
