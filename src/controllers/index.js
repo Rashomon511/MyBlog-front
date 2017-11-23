f62b293a37a87a4cf995affef9e3027b25ad2289
LuoShengMen <15271835932@163.com>
2017-11-23 22:58:00 +0800

    封装一个fetch方法

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,8 +1,5 @@
-import axios from 'axios';
-axios.defaults.baseURL = 'http://192.168.10.135:3001';
-/*axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;*/
-axios.defaults.headers.post['content-Type'] = 'appliction/x-www-form-urlencoded';
+import fetch from '../utils/index';
 
 export const GetUser = () => {
-    return axios.get('/user/')
+    return fetch({url:'/user'})
 }
---
+++ src/utils/index.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const fetch = axios.create({
+    baseURL: 'http://192.168.10.135:3001', // api的base_url
+    timeout: 500000                  // 请求超时时间
+})
+
+fetch.interceptors.request.use(config => {
+    // Do something before request is sent
+    config.headers['Content-Type'] = 'appliction/x-www-form-urlencoded'
+    //config.headers['Authorization'] = getToken() // 让每个请求携带token--['Authorization']
+    return config
+}, error => {
+    // Do something with request error
+    console.log(error); // for debug
+    Promise.reject(error)
+})
+
+// respone拦截器
+fetch.interceptors.response.use(
+    response => response,
+    error => {
+        console.log('err' + error);// for debug
+        return Promise.reject(error)
+    }
+)
+
+export default fetch
