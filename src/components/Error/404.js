80285938c3130a7b91b5fd16b68ba8bc1da2c9f4
LuoShengMen <15271835932@163.com>
2017-12-02 23:40:22 +0800

    404页面

--- src/components/Error/404.js
+++ src/components/Error/404.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Button } from 'antd';
+import style from './index.less';
 
 class ErrorPage extends React.Component {
-    constructor() {
-        super();
-        this.state = {}
-    }
+    static contextTypes = {
+        router: PropTypes.object
+    };
+
+    Click = () => {
+      return this.context.router.push('/main/article')
+    };
 
     render() {
         return (
-            <div>
-                404
+            <div className={style.wrap}>
+               <Button onClick={this.Click} className={style.btn} type='primary'>返回首页</Button>
             </div>
         );
     }
