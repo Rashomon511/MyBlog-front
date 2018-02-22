3ed1c45423462727e7bc43dad581af8985c5a8a1
rashomon <rashomon511@gmail.com>
2018-02-22 18:04:04 +0800

    产品环境webpack配置

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -3,6 +3,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const OfflinePlugin = require('offline-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ProgressBarPlugin = require('progress-bar-webpack-plugin');
@@ -82,16 +83,45 @@ module.exports = {
         new webpack.optimize.CommonsChunkPlugin({
             name: "vendor",
             minChunks: Infinity,
-        }),
+        }), // 提取公共模块，然后将公共模块打包到 vendor.js。
         new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
             filename: 'index.html',
-            template: 'public/index.html'
+            template: 'public/index.html',
+            minify: {
+                removeComments: true, // 去掉注释信息
+                collapseWhitespace: true, // 合并空白符
+                removeRedundantAttributes: true, // 去掉多余的属性，比如某些默认属性
+                useShortDoctype: true, // 使用 <!DOCTYPE html>
+                removeEmptyAttributes: true, // 去掉空白的属性，比如 id = " "
+                removeStyleLinkTypeAttributes: true, // 去掉 style 和 link 标签的 type="text/css"
+                keepClosingSlash: true, // 保持标签闭合
+                minifyJS: true, // 压缩 JavaScript (使用 UglifyJS)
+                minifyCSS: true, // 压缩 CSS (uses clean-css)
+                minifyURLs: true, // 压缩 URLs (uses relateurl)
+            },
+            inject: true, // 制定资源存放位置
         }),
         new ExtractTextPlugin('style.css', { allChunks: true }),
-        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.OccurrenceOrderPlugin(true), // 排序输出
+        //new webpack.optimize.DedupePlugin(), // 删除与NPM重复的数据，有效减小文件大小
         new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-        new ProgressBarPlugin()
+        new ProgressBarPlugin(),
+        // 离线缓存机制
+        // new OfflinePlugin({
+        //     excludes: ['.htaccess'],
+        //
+        //     caches: {
+        //         main: [':rest:'],
+        //         additional: ['*.chunk.js'],
+        //     },
+        //
+        //     safeToUseOptionalCaches: true,
+        //
+        //     AppCache: {
+        //         caches: ['main', 'additional'],
+        //     },
+        // }),
     ],
     node: {
         dgram: 'empty',
